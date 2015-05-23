5ddb92d7e2ae9fb7ad48c0776794183c7433a339
Aleuck <aleuck@github.com>
2015-05-23 18:02:05 -0300

    first commit

---
+++ bayes.py
@@ -0,0 +1,42 @@
+import linecache
+import csv
+import scipy
+import numpy
+
+class DigitClass:
+    def __init__(self, digit):
+        self.digit = digit
+        self.pixels = [[] for i in range(256)]
+        self.means = [-1 for i in range(256)]
+        self.variances = [-1 for i in range(256)]
+        self.instances = 0
+
+def get_class_list():
+    idx_file = open('indices/2.csv', 'r')
+    idx_reader = csv.reader(idx_file)
+    class_list = [DigitClass(i) for i in range(10)]
+    # For each of the student's selected indexes
+    for row in idx_reader:
+        # Readlist corresponding line on usps database
+        usps_line = linecache.getline('data/usps.csv', int(row[0]) + 1)
+        usps_line = list(csv.reader([usps_line], delimiter=','))[0]
+
+        # get class reference
+        digit_class = class_list[int(usps_line.pop())]
+        digit_class.instances += 1
+
+        # add each pixel value
+        for i in range(256):
+            digit_class.pixels[i].append(float(usps_line[i]))
+
+    # For each class, calculate the mean and variance for each pixel
+    for digit_class in class_list:
+        for i in range(256):
+            digit_class.means[i] = numpy.mean(digit_class.pixels[i])
+            digit_class.variances[i] = numpy.var(digit_class.pixels[i])
+
+    return class_list
+
+class_list = get_class_list()
+for digit_class in class_list:
+    print(str(digit_class.digit) + "(" + str(digit_class.instances) + "): " + str(numpy.mean(digit_class.means)))
