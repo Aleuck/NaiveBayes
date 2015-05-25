dac6faaa1eef279a013381d5de2a2ab2277a4fcd
felipeal <limalipe@hotmail.com>
2015-05-24 21:23:38 -0300

    Mostly working

--- bayes.py
+++ bayes.py
@@ -1,15 +1,35 @@
+from scipy import stats
 import linecache
 import csv
-import scipy
 import numpy
+
+
+
+##################################################
+## Classes
+##################################################
 
 class DigitClass:
     def __init__(self, digit):
         self.digit = digit
-        self.pixels = [[] for i in range(256)]    # pixels[0..255]
+        self.pixels = [[] for i in range(256)]    # pixels[0..255][sample]
         self.means = [-1 for i in range(256)]     # means[0..255]
         self.variances = [-1 for i in range(256)] # variances[0..255]
         self.num_samples = 0
+    
+    def get_similarity(self, pixels):
+        logpdfs = []
+        
+        for i in range(256):
+            logpdfs.append(stats.norm.logpdf(pixels[i], loc=self.means[i], scale=self.variances[i]))
+        
+        return numpy.sum(logpdfs)
+
+
+
+##################################################
+## Functions
+##################################################
 
 def get_class_list():
     idx_file = open('data/2.csv', 'r')
@@ -39,6 +59,49 @@ def get_class_list():
     
     return class_list
 
+def get_sample_pixels(class_list, class_index, sample_index):
+    sample_pixels = []
+    
+    for i in range(256):
+        sample_pixels.append(class_list[class_index].pixels[i][sample_index])
+    
+    return sample_pixels
+    
+def get_most_likely_digit(class_list, sample_pixels):
+    
+    similarity_digit = []
+    similarity_value = []
+    
+    for digit_class in class_list:
+        
+        similarity_digit.append(digit_class.digit)
+        similarity_value.append(digit_class.get_similarity(sample_pixels))
+    
+    # Sort digits by similarity value
+    sorted_digits = sorted(similarity_digit, key=lambda i: similarity_value[i], reverse=True)
+    return sorted_digits[0]
+    
+
+
+##################################################
+## Main
+##################################################
+
 class_list = get_class_list()
-for digit_class in class_list:
-    print(str(digit_class.digit) + ": " + str(numpy.mean(digit_class.means)))
+#for digit_class in class_list:
+#    print(str(digit_class.digit) + ": " + str(numpy.mean(digit_class.means)))
+
+samples_per_class = 20 # Change for faster/slower tests
+
+for class_index in range(10):
+    right_guesses = 0
+    
+    for sample_index in range(samples_per_class):
+        
+        sample_pixels = get_sample_pixels(class_list, class_index, sample_index)
+        most_likely_digit = get_most_likely_digit(class_list, sample_pixels)
+        
+        if (most_likely_digit == class_index):
+            right_guesses += 1
+    
+    print(str(class_index) + ": " + str(right_guesses) + "/" + str(samples_per_class))
